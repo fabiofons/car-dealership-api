4699746fd2163e1c167bdeb62dd79a07846bc843
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-10-27 08:51:42 -0500

    Inyección de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-  private cars: string[] = ['Toyota', 'Jeep', 'Honda'];
+  constructor(
+    private readonly carsService: CarsService
+  ) { }
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAllCars();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
     console.log({ id });
-    return this.cars[id];
+    return this.carsService.findCarById(+id);
   }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+  findAllCars() {
+    return this.cars;
+  }
+
+  findCarById(id: number) {
+    return this.cars.filter(car => car.id === id);
+  }
+}
