4706a82b1a2e32094fd307c6d1c8e4ea30d8291f
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2024-10-27 08:09:50 -0500

    return car by id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+  private cars: string[] = ['Toyota', 'Jeep', 'Honda'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Jeep', 'Honda']
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({ id });
+    return this.cars[id];
   }
 }
